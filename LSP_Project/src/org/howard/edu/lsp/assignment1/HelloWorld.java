5135fa37bbfb905da7165e67a746e7c2d39a1f2d
vin3i <vin3i@LAPTOP-9TANBQ0M.mynetworksettings.com>
2025-08-22 10:48:06 -0400

    This is assignment one.

---
+++ LSP_Project/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Carolyn Winn
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
